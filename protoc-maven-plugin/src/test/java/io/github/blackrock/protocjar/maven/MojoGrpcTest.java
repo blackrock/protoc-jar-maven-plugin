2ee1b9251e1f001568c4f8fbebe87846288ff8db
spensonshih <spenson_shih@yahoo.com>
2024-05-03 10:57:03 +0100

    Test against Maven 3.6.x, 3.8.x, and 3.9.x

--- protoc-maven-plugin/src/test/java/io/github/blackrock/protocjar/maven/MojoGrpcTest.java
+++ protoc-maven-plugin/src/test/java/io/github/blackrock/protocjar/maven/MojoGrpcTest.java
@@ -28,7 +28,7 @@ import io.takari.maven.testing.executor.MavenVersions;
 import io.takari.maven.testing.executor.junit.MavenJUnitTestRunner;
 
 @RunWith(MavenJUnitTestRunner.class)
-@MavenVersions({"3.2.3"})
+@MavenVersions({"3.6.3", "3.8.8", "3.9.6"})
 public class MojoGrpcTest
 {
 	@Rule
